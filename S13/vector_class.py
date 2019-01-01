194eb5d581a5d9cd39e30e9d3ba14718d80eb5ad
Vahid Naeini <naeini.v@gmail.com>
2019-01-01 20:20:56 +0330

    oop python files 2

--- S13/vector_class.py
+++ S13/vector_class.py
@@ -6,8 +6,15 @@ class Vector:
     def length(self):
         return (self.x ** 2 + self.y ** 2) ** 0.5
 
+    def __add__(self, other):
+        x_new = self.x + other.x
+        y_new = self.y + other.y
+        return Vector(x_new, y_new)
+
 v1 = Vector(1, 2)
 v2 = Vector(5, 8)
 
+v3 = v1 + v2    #  v1.__add__(v2)
 print(v1.x, v1.y, v1.length())
 print(v2.x, v2.y, v2.length())
+print(v3.x, v3.y, v3.length())
