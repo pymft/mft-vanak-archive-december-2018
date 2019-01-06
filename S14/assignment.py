c8b1a50e1831140371aae0701e62a47724c40a53
Vahid Naeini <naeini.v@gmail.com>
2019-01-06 20:07:49 +0330

    stars :star:

--- S14/assignment.py
+++ S14/assignment.py
@@ -6,9 +6,22 @@ class Stars:
 
     def __repr__(self):
         if self.fill:
-            pass
+            out = ''
+            for i in range(self.n):
+                line = self.pat * (2*i+1)
+                out += line.center(2*self.n+1)
+                out += '\n'
+            return out
         else:
-            pass
+            out = ''
+            out += '*'.center(2*self.n+1)
+            out += '\n'
+            for i in range(1, self.n-1):
+                line = self.pat + ' ' * (2*i-1) + self.pat
+                out += line.center(2*self.n+1)
+                out += '\n'
+            out += self.pat * (2*self.n+1)
+            return out
 
 s1 = Stars(2)
 s1
