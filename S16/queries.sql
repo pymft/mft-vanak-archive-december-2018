ed5f7c185f991f0799d1ff57d462c204100bc2f4
Vahid Naeini <naeini.v@gmail.com>
2019-01-13 18:11:57 +0330

    sqlite queries

---
+++ S16/queries.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO student (firstname, lastname, age) VALUES ("John", "Smith", 20);
+-- INSERT INTO student (firstname, lastname) VALUES ("Emma", "Watson");
+
+SELECT firstname as "نام", lastname as "نام خانوادگی", age FROM student;
+-- SELECT id, firstname , lastname, age FROM student where age > 12;
+-- update student set age = 23 ;
