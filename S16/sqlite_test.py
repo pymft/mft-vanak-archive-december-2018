f9ae047c685481a37971a137fe977a0adbce4f50
Vahid Naeini <naeini.v@gmail.com>
2019-01-13 18:21:43 +0330

    sqlite queries part 2

--- S16/sqlite_test.py
+++ S16/sqlite_test.py
@@ -2,5 +2,11 @@ import sqlite3
 
 
 conn = sqlite3.connect("my-db.sqlite")
-conn.execute('INSERT INTO student (firstname, lastname, age) VALUES ("Emma", "Watson", 28);')
-# conn.commit()
+fname = input("what's your first name? ")
+lname = input("what's your last name? ")
+age = input("how old are you? ")
+# fname = 'john", "smith", 20); ....; INSERT INTO student (firstname, lastname, age) VALUES ("John'
+cmd = 'INSERT INTO student (firstname, lastname, age) VALUES ("' + fname + '", "' + lname + '", ' + age + ');'
+conn.execute(cmd)
+
+conn.commit()
