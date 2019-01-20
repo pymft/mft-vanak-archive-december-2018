4424f57565f3694bff253115d466c469de154611
Vahid Naeini <naeini.v@gmail.com>
2019-01-20 20:19:31 +0330

    more flask jinja and also pygal to show charts :cool:

--- S18/flask_app.py
+++ S18/flask_app.py
@@ -1,20 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import pygal
 
 
 app = Flask(__name__)
 
+@app.route('/chart/<int:myvalue>')
+def chart(myvalue):
+    pie_chart = pygal.Pie()
+    pie_chart.title = 'Browser usage in February 2012 (in %)'
+    pie_chart.add('IE', 19.5)
+    pie_chart.add('Firefox', 36.6)
+    pie_chart.add('Chrome', 36.3)
+    pie_chart.add('Safari', 4.5)
+    pie_chart.add('Opera', 2.3)
+
+    pie_chart.add('My Value', myvalue)
+    return render_template('charts.html', chart=pie_chart.render_data_uri())
 
 @app.route('/')
 def hello():
     return render_template("login.html")
 
+@app.route('/login', methods=["GET", "POST"])
+@app.route('/login/<color>', methods=["GET", "POST"])
+def login(color="008888"):
+    if request.method == 'GET':
+        color = "#" + color
+        return render_template("enroll.html", color=color)
+    if request.method == 'POST':
+        return redirect("/test")
 
-@app.route('/login')
-def login():
-    return render_template("sample.html")
-
+@app.route('/test')
 @app.route('/test/<int:m>/<int:n>')
-def something(m, n):
+def something(m=10, n=10):
     # text = "<h1> hello </h1>" * num
 
     return render_template("test.html", m=m, n=n)
@@ -24,4 +42,4 @@ def something(m, n):
 
 
 
-app.run()
+app.run(host="0.0.0.0", port=8000)
